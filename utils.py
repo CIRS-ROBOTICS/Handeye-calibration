c7e52273cc170761de882233d4e1e54f1d3dfbaf
barry <barry1997@126.com>
2024-09-26 19:45:02 +0800

    add the function realted to global ICP

--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ import rtde_receive
 from real_lab.end_effector import robotiq_gripper
 import open3d as o3d
 import copy
+from scipy.spatial.transform import Rotation as R 
 
 
 def build_workspace_points(workspace_limits, calib_grid_step):
@@ -39,7 +40,7 @@ def init_robot(robot_ip, camera_model):
     # initialize the gripper
     gripper = robotiq_gripper.RobotiqGripper() # Creating gripper
     gripper.connect(robot_ip, 63352)    # Connecting to gripper
-    gripper.activate()                         
+    # gripper.activate()                         
 
     # set up camera
     camera = RealSense(model=camera_model)
@@ -100,3 +101,56 @@ def draw_registration_result(source, target, transformation):
     o3d.visualization.draw_geometries(
         [source_temp,
          target_temp])
+    
+def get_checkboard_position(rtde_r, gripper):
+    gripper.move_and_wait_for_pos(255, 255, 255) # close gripper
+    
+    print('Please drag the robot arm to the checkboard center position')
+    print('Press any key to confirm the position')
+    press_any_key()
+
+    tool_offest = 0.0135 # NOTE: The deviation from the actual state on the z axis after the gripper is closed
+
+    cur_state = rtde_r.getActualTCPPose()
+    checkboard_center = cur_state[:3]
+    checkboard_center[2] -= tool_offest # Correction of z-axis deviation
+
+    print('Checkboard position: [%f, %f, %f]' % tuple(checkboard_center))
+    return checkboard_center
+
+def get_gripper2base(rtde_r):
+    pose = rtde_r.getActualTCPPose()
+    mat = R.from_rotvec(pose[3:]).as_matrix() # rotation matrix of gripper to base 3*3
+    trans = pose[:3]
+    # trans = np.expand_dims(trans,axis=0)
+    # T_g2b = np.concatenate((mat, trans.T),axis=1)
+    # T_g2b = np.concatenate((T_g2b, np.array([[0,0,0,1]])),axis=0)
+
+    return trans, mat
+
+def preprocess_point_cloud(pcd, voxel_size):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
+
+def execute_fast_global_registration(source_down, target_down, source_fpfh,
+                                     target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 0.5
+    print(":: Apply fast global registration with distance threshold %.3f" \
+            % distance_threshold)
+    result = o3d.pipelines.registration.registration_fgr_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh,
+        o3d.pipelines.registration.FastGlobalRegistrationOption(
+            maximum_correspondence_distance=distance_threshold))
+    return result
